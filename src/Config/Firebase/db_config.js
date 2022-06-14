b0b40ea4d03c352ecdf5c7a4e347f88ccfbf2018
Carlos Kelley <carloskelley@protonmail.com>
2022-06-14 02:18:24 -0500

    removed firebase cruft

--- src/Config/Firebase/db_config.js
+++
@@ -1,9 +0,0 @@
-// export const DB_CONFIG = {
-//     apiKey: "AIzaSyCe__409C0TDLgrJ4K8RzhkK2u1XQmhtTc",
-//   authDomain: "flashcards-543c9.firebaseapp.com",
-//   projectId: "flashcards-543c9",
-//   storageBucket: "flashcards-543c9.appspot.com",
-//   messagingSenderId: "473487314649",
-//   appId: "1:473487314649:web:34bacd8a64ec62c67978c8",
-//   measurementId: "G-27NJE8P4CR"
-// }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Search from '../Search/Search';
-//import firebase from 'firebase/app';
-//import 'firebase/database'; 
-//import { initializeApp } from 'firebase/app';
 
-
-//import {DB_CONFIG} from '../../Config/Firebase/db_config';
 
 function App() {
-  //this.app = firebase.initializeApp(DB_CONFIG);
-  //this.database = this.app.database().ref().child('flashcards');
+
   return (
     
     <div className="App">
